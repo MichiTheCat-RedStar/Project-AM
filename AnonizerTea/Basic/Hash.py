1f9f40a02102280411c28b5ed163d9acd28b6325
Michi The Cat <109546537+MichiTheCat-RedStar@users.noreply.github.com>
2026-05-19 22:32:46 +0300

    v0.1b
    Надеюсь ничего не забыл...

---
+++ AnonizerTea/Basic/Hash.py
@@ -0,0 +1,7 @@
+#import hashlib
+#
+#while True:
+#	text = input('>>> ')
+#	hash_object = hashlib.sha512(text.encode())
+#	hex_digest = hash_object.hexdigest()
+#	print('\n', hex_digest, '\n')
---
+++ AnonizerTea/Basic/Xor.py
@@ -0,0 +1,4 @@
+#def xor_bytes(b, key):
+#    return bytes(x ^ key[i % len(key)] for i, x in enumerate(b))
+#while True:
+#    print(xor_bytes(b'test', b'wd')); break
