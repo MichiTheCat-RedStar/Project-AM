02aceffad983cbee93db4c43811b413deded1234
Michi The Cat <109546537+MichiTheCat-RedStar@users.noreply.github.com>
2026-06-08 19:32:59 +0300

    Delete AnonizerTea directory

--- AnonizerTea/Basic/Xor.py
+++
@@ -1,4 +0,0 @@
-#def xor_bytes(b, key):
-#    return bytes(x ^ key[i % len(key)] for i, x in enumerate(b))
-#while True:
-#    print(xor_bytes(b'test', b'wd')); break
--- AnonizerTea/TeaCoder_File2Str/TeaCoder2_File2Str_GUI.py
+++
@@ -1,2 +0,0 @@
-#    TeaCoder_File2Str // ☭
-# MichiTheCat-RedStar (c) 2026
